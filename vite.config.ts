80c9d56dbf9dfab648e42f2ef649d094b2aed38c
Alexey <alexeykirillov@oulook.com>
2026-01-21 18:56:44 +0100

    fix for vercel

--- vite.config.ts
+++ vite.config.ts
@@ -254,7 +254,6 @@ export default defineConfig(({ mode }) => {
           faq: resolve(__dirname, 'faq.html'),
           privacy: resolve(__dirname, 'privacy.html'),
           terms: resolve(__dirname, 'terms.html'),
-          licensing: resolve(__dirname, 'licensing.html'),
           tools: resolve(__dirname, 'tools.html'),
           '404': resolve(__dirname, '404.html'),
           // Category Hub Pages
